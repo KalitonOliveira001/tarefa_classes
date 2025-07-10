6de07046c8ad063b297494ec4c87cde484f0b5f4
KalitonOliveira001 <analistadeDadoskaliton@gmail.com>
2025-07-10 00:57:29 -0300

    Add files via upload

---
+++ Cadeira.java
@@ -0,0 +1,18 @@
+// Classe que representa uma cadeira
+public class Cadeira {
+    
+    // Propriedades (atributos)
+    String cor;
+    int pernas;
+    boolean temEncosto;
+
+    // Método para sentar
+    public void sentar() {
+        System.out.println("Você está sentado na cadeira.");
+    }
+
+    // Método para mover
+    public void mover() {
+        System.out.println("Você moveu a cadeira.");
+    }
+}
